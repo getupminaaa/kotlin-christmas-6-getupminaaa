0c4cac2f96d72b4f1c275496319ceadac49c0081
getupminaaa <getupmina0405@gmail.com>
2023-11-10 16:57:54 +0900

    refactor: 입력 받는 값을 외부에서는 수정이 불가능하도록 수정

--- src/main/kotlin/christmas/view/InputView.kt
+++ src/main/kotlin/christmas/view/InputView.kt
@@ -4,12 +4,21 @@ import camp.nextstep.edu.missionutils.Console
 
 class InputView {
 
-    fun getRestaurantVisitDate(){
-        println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)")
-        val input = Console.readLine()
+    val inputDate: String
+        get() = _inputDate
+    private var _inputDate = ""
+
+    init {
+        _inputDate = getVisitDate()
     }
 
-    fun getOrderedMenu(){
+
+    private fun getVisitDate(): String {
+        println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)")
+        return Console.readLine()
+    }
+
+    fun getOrderedMenu() {
         println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)")
         val input = Console.readLine()
     }
