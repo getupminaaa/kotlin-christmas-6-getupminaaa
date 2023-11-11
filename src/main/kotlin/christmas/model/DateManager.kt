74ec764afe6b75732aace4c045273e4046a5a0f2
getupminaaa <getupmina0405@gmail.com>
2023-11-11 15:41:15 +0900

    feat: 크리스마스디데이이벤트에 사용되는 날짜 계산기능 구현

--- src/main/kotlin/christmas/model/DateManager.kt
+++ src/main/kotlin/christmas/model/DateManager.kt
@@ -2,15 +2,19 @@ package christmas.model
 
 
 class DateManager(private val validDate: Int) {
-    val visitDate: Int
-        get() = _visitDate
-    private var _visitDate = 0
+
+    val dDayEventDate: Int
+        get() = _dDayEventDate
+    private var _dDayEventDate = 0
 
     init {
-        setVisitDate()
+        _dDayEventDate = calDDayEventDate()
     }
 
-    private fun setVisitDate() {
-        _visitDate = validDate
+    //d-day 계산해서 넘김 (이벤트 매니저한테)
+    private fun calDDayEventDate(): Int {
+        return validDate - 1
     }
+
+    // 평일, 주말, 특별인지
 }
