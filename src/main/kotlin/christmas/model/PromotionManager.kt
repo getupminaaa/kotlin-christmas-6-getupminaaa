130aafe7ca32fe01ad820a6aff112133bef17abf
getupminaaa <getupmina0405@gmail.com>
2023-11-12 14:54:50 +0900

    feat: 방문 날짜에 따라 이벤트를 구분하는 기능 구현

--- src/main/kotlin/christmas/model/PromotionManager.kt
+++ src/main/kotlin/christmas/model/PromotionManager.kt
@@ -1,8 +1,30 @@
 package christmas.model
 
+import christmas.util.EventType
+
 class PromotionManager(private val validDate:Int,private val totalPrice: Int) {
 
-    private fun areAllEventsApplicable(): Boolean = (totalPrice >= 10000)
+    private val weekend = mapOf(EventType.STAR_DAY to listOf(1, 2, 8, 9, 15, 16, 22, 23, 29, 30))
+    private val weekDays = mapOf(EventType.WEEKDAYS to listOf(4, 5, 6, 7, 11, 12, 13, 14, 18, 19, 20, 21, 25, 26, 27, 28))
+    private val starDays = mapOf(EventType.WEEKEND to listOf(3, 10, 17, 24, 25, 31))
+    val applicableEvents: MutableList<String>
+        get() = _applicableEvents
+    private var _applicableEvents = mutableListOf<String>()
 
-    
+
+    private fun areAllEventsApplicable(): Boolean = (totalPrice >= 10000)
+    private fun calDateEvent() {
+        when {
+            validDate == 25 -> _applicableEvents.addAll(listOf(EventType.WEEKDAYS.name,EventType.STAR_DAY.name))
+            weekDays.flatMap { it.value }.contains(validDate) -> _applicableEvents.add(EventType.WEEKDAYS.name)
+            starDays.flatMap { it.value }.contains(validDate) -> _applicableEvents.add(EventType.STAR_DAY.name)
+            else -> _applicableEvents.add(EventType.WEEKEND.name)
+        }
+    }
+    private fun checkApplicableEvent() {
+        if (areAllEventsApplicable()) {
+            calDateEvent()
+
+        }
+    }
 }
