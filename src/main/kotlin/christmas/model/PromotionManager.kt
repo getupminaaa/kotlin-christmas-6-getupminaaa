8242a36c1ada1e073de9e481e0fef57821e82d6e
getupminaaa <getupmina0405@gmail.com>
2023-11-12 14:51:12 +0900

    feat: 총 금액이 10000원 이상일 때 혜택을 받을 수 있도록 구현

--- src/main/kotlin/christmas/model/PromotionManager.kt
+++ src/main/kotlin/christmas/model/PromotionManager.kt
@@ -1,11 +1,8 @@
 package christmas.model
 
-class PromotionManager(private val validDate:Int) {
-    
-    //d-day 계산해서 넘김 (이벤트 매니저한테)
-    private fun calDDayEventDate(): Int {
-        return validDate - 1
-    }
+class PromotionManager(private val validDate:Int,private val totalPrice: Int) {
 
-    // 평일, 주말, 특별인지
+    private fun areAllEventsApplicable(): Boolean = (totalPrice >= 10000)
+
+    
 }
