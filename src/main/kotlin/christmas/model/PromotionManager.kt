3a02de4ec3f6215dafdbdf34f994f12baae6dd9f
getupminaaa <getupmina0405@gmail.com>
2023-11-12 15:00:07 +0900

    feat: 총 주문 금액이 12만원 이상인지에 따라 증정 여부를 결정하는 기능 구현

--- src/main/kotlin/christmas/model/PromotionManager.kt
+++ src/main/kotlin/christmas/model/PromotionManager.kt
@@ -4,9 +4,9 @@ import christmas.util.EventType
 
 class PromotionManager(private val validDate:Int,private val totalPrice: Int) {
 
-    private val weekend = mapOf(EventType.STAR_DAY to listOf(1, 2, 8, 9, 15, 16, 22, 23, 29, 30))
     private val weekDays = mapOf(EventType.WEEKDAYS to listOf(4, 5, 6, 7, 11, 12, 13, 14, 18, 19, 20, 21, 25, 26, 27, 28))
-    private val starDays = mapOf(EventType.WEEKEND to listOf(3, 10, 17, 24, 25, 31))
+    private val starDays = mapOf(EventType.STAR_DAY to listOf(3, 10, 17, 24, 25, 31))
+
     val applicableEvents: MutableList<String>
         get() = _applicableEvents
     private var _applicableEvents = mutableListOf<String>()
@@ -14,8 +14,22 @@ class PromotionManager(private val validDate:Int,private val totalPrice: Int) {
         get() = _dDayEventDate
     private var _dDayEventDate = 0
 
+    init {
+        checkApplicableEvent()
+    }
 
     private fun areAllEventsApplicable(): Boolean = (totalPrice >= 10000)
+
+    private fun isDDayEventApplicable() {
+        if (validDate <= 25) {
+            _applicableEvents.add(EventType.D_DAY.name)
+            calDDayEventDate()
+        }
+    }
+    private fun calDDayEventDate() {
+        _dDayEventDate = validDate - 1
+    }
+
     private fun calDateEvent() {
         when {
             validDate == 25 -> _applicableEvents.addAll(listOf(EventType.WEEKDAYS.name,EventType.STAR_DAY.name))
@@ -24,23 +38,17 @@ class PromotionManager(private val validDate:Int,private val totalPrice: Int) {
             else -> _applicableEvents.add(EventType.WEEKEND.name)
         }
     }
-    private fun isDDayEventApplicable() {
-        if (validDate <= 25) {
-            _applicableEvents.add(EventType.D_DAY.name)
-            calDDayEventDate()
-        }
-    }
 
-    private fun calDDayEventDate() {
-        _dDayEventDate = validDate - 1
-    }
+    private fun isGiftEventApplicable() {
+        if (totalPrice >= 120000) _applicableEvents.add(EventType.FREE_GIFT.name)
 
+    }
 
     private fun checkApplicableEvent() {
         if (areAllEventsApplicable()) {
-            calDateEvent()
             isDDayEventApplicable()
+            calDateEvent()
+            isGiftEventApplicable()
         }
     }
-
 }
