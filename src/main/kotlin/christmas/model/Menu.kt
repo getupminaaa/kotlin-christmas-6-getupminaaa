8788fcbd10a3446e45fae75579bbd2ff65169918
getupminaaa <getupmina0405@gmail.com>
2023-11-12 15:06:43 +0900

    refactor: enum class의 상수들을 영어로 수정

--- src/main/kotlin/christmas/model/Menu.kt
+++ src/main/kotlin/christmas/model/Menu.kt
@@ -2,29 +2,29 @@ package christmas.model
 
 class Menu {
     enum class MenuCategory {
-        애피타이저,
-        메인,
-        디저트,
-        음료
+        APPETIZER,
+        MAIN_DISHES,
+        DESSERT,
+        BEVERAGE
     }
 
     private val menuBoard = mapOf(
-        MenuCategory.애피타이저 to listOf(
+        MenuCategory.APPETIZER to listOf(
             MenuItem("양송이수프", 6000),
             MenuItem("타파스", 5500),
             MenuItem("시저샐러드", 8000)
         ),
-        MenuCategory.메인 to listOf(
+        MenuCategory.MAIN_DISHES to listOf(
             MenuItem("티본스테이크", 55000),
             MenuItem("바비큐립", 54000),
             MenuItem("해산물파스타", 35000),
             MenuItem("크리스마스파스타", 25000)
         ),
-        MenuCategory.디저트 to listOf(
+        MenuCategory.DESSERT to listOf(
             MenuItem("초코케이크", 15000),
             MenuItem("아이스크림", 5000)
         ),
-        MenuCategory.음료 to listOf(
+        MenuCategory.BEVERAGE to listOf(
             MenuItem("제로콜라", 3000),
             MenuItem("레드와인", 60000),
             MenuItem("샴페인", 25000)
@@ -36,7 +36,7 @@ class Menu {
     }
 
     fun areItemsNotOnlyBeverage(itemNames: List<String>): Boolean {
-        val beverageMenu = menuBoard[MenuCategory.음료]!!.map { it.name }
+        val beverageMenu = menuBoard[MenuCategory.BEVERAGE]!!.map { it.name }
         return itemNames.subtract(beverageMenu.toSet()).isNotEmpty() //음료만으로 구성되어있지 않음 => true
     }
 
