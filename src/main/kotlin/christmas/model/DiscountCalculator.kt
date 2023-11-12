4153ca742be49122827b0f4bd805502bf8259ade
getupminaaa <getupmina0405@gmail.com>
2023-11-12 17:02:26 +0900

    feat: 크리스마스 디데이 할인금액 계산 구현

--- src/main/kotlin/christmas/model/DiscountCalculator.kt
+++ src/main/kotlin/christmas/model/DiscountCalculator.kt
@@ -4,9 +4,14 @@ class DiscountCalculator(
     private val applicableEvents: List<String>,
     private val categoryQuantities: List<Map<String, Int>>
 ) {
+    private var dDayEventAmount = 0
 
     // -- 할인 --
     //크리스마스 디데이 할인 계산
+    fun discountDDay(dDayEventDate: Int) {
+        dDayEventAmount = 1000 + dDayEventDate * 100
+    }
+
     //  평일할인 계산
     //  특별할인 계산
     //  주말할인 계산
