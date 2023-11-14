53285d9f6b0cfd84a9cd18d6b9fd77e35bedf149
getupminaaa <getupmina0405@gmail.com>
2023-11-14 15:47:22 +0900

    test: dDayEventDate 값이 올바른지 확인하는 테스트 작성

--- src/test/kotlin/christmas/model/EventTest.kt
+++ src/test/kotlin/christmas/model/EventTest.kt
@@ -20,13 +20,28 @@ class EventTest {
     @Test
     @DisplayName("금액에 따른 eventBadge값이 올바른지 확인")
     fun getEventBadgeTest() {
-        val totalDiscount = listOf(5000,9000,10000,19000,20000,30000)
-        val expected = listOf("별","별","트리","트리","산타","산타")
+        val totalDiscounts = listOf(5000, 9000, 10000, 19000, 20000, 30000)
+        val expected = listOf("별", "별", "트리", "트리", "산타", "산타")
         val actualValues = mutableListOf<String>()
-        totalDiscount.forEach {
+        totalDiscounts.forEach {
             event.getEventBadgeType(it)
             actualValues.add(event.eventBadge)
         }
-        assertEquals(expected,actualValues)
+        assertEquals(expected, actualValues)
     }
+
+    @Test
+    @DisplayName("dDayEventDate 값이 올바른지 확인")
+    fun compareDDayEventDate() {
+        val validDates = listOf(3, 10, 21, 25)
+        val expected = listOf(2, 9, 20, 24)
+        val actualValues = mutableListOf<Int>()
+        event.applicableEvents
+        validDates.forEach {
+            val newEvent = Event(it, 10000)
+            actualValues.add(newEvent.dDayEventDate)
+        }
+        assertEquals(expected, actualValues)
+    }
+
 }
