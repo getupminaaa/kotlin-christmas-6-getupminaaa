723f5006027af28e244e3bc79d717a34c64e14e8
getupminaaa <getupmina0405@gmail.com>
2023-11-14 14:17:59 +0900

    test: 공통적으로 사용되는 변수를 메서드 밖의 private val 변수로 수정

--- src/test/kotlin/christmas/model/MenuBoardTest.kt
+++ src/test/kotlin/christmas/model/MenuBoardTest.kt
@@ -8,6 +8,14 @@ import org.junit.jupiter.api.Test
 
 class MenuBoardTest {
     private val menuBoard = MenuBoard()
+    private val validOrder = listOf(
+        mapOf("타파스" to 1),
+        mapOf("초코케이크" to 1),
+        mapOf("레드와인" to 1),
+        mapOf("해산물파스타" to 1),
+        mapOf("바비큐립" to 1),
+        mapOf("시저샐러드" to 1)
+    )
 
     @Test
     @DisplayName("메뉴판에 있을 경우 true, 없을 경우 false를 반환하는지")
@@ -21,7 +29,7 @@ class MenuBoardTest {
     @Test
     @DisplayName("음료로만 구성되어있지 않을 경우 true, 음료로만 구성되어있을 경우 false를 반한하는지")
     fun areItemsNotOnlyBeverageTest() {
-        assertFalse { menuBoard.areItemsNotOnlyBeverage(listOf("제로콜라","레드와인","샴페인")) }
+        assertFalse { menuBoard.areItemsNotOnlyBeverage(listOf("제로콜라", "레드와인", "샴페인")) }
         assertTrue { menuBoard.areItemsNotOnlyBeverage(listOf("제로콜라", "타파스", "초코케이크")) }
         assertTrue { menuBoard.areItemsNotOnlyBeverage(listOf("티본스테이크", "타파스", "초코케이크")) }
     }
@@ -29,14 +37,6 @@ class MenuBoardTest {
     @Test
     @DisplayName("입력된 메뉴들에 따른 가격을 잘 반환하는지")
     fun getMenuItemPriceTest() {
-        val validOrder = listOf(
-            mapOf("타파스" to 1),
-            mapOf("초코케이크" to 1),
-            mapOf("레드와인" to 1),
-            mapOf("해산물파스타" to 1),
-            mapOf("바비큐립" to 1),
-            mapOf("시저샐러드" to 1)
-        )
         val expected = listOf(5500, 15000, 60000, 35000, 54000, 8000)
         val actualValue = menuBoard.getMenuItemPrices(validOrder)
         assertEquals(expected, actualValue)
@@ -45,14 +45,6 @@ class MenuBoardTest {
     @Test
     @DisplayName("입력된 여러 메뉴들에 대해 메뉴아이템을 잘 반환하는지")
     fun getTypesOfMenuItemsTest() {
-        val validOrder = listOf(
-            mapOf("타파스" to 1),
-            mapOf("초코케이크" to 1),
-            mapOf("레드와인" to 1),
-            mapOf("해산물파스타" to 1),
-            mapOf("바비큐립" to 1),
-            mapOf("시저샐러드" to 1)
-        )
         val expected = listOf(
             MenuItem(MenuCategory.APPETIZER.name, "타파스", 5500),
             MenuItem(MenuCategory.DESSERT.name, "초코케이크", 15000),
@@ -62,7 +54,6 @@ class MenuBoardTest {
             MenuItem(MenuCategory.APPETIZER.name, "시저샐러드", 8000)
         )
         val actualValue = menuBoard.getMenuItems(validOrder)
-
         assertEquals(expected, actualValue)
     }
 }
