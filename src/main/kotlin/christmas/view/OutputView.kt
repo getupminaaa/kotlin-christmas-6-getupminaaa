9b875aeffca76889e631541d76c474b3ba8319db
getupminaaa <getupmina0405@gmail.com>
2023-11-14 13:37:16 +0900

    refactor: outputView의 printEventBadge 메서드 수정

--- src/main/kotlin/christmas/view/OutputView.kt
+++ src/main/kotlin/christmas/view/OutputView.kt
@@ -7,47 +7,54 @@ class OutputView {
         printStartMsg()
     }
 
-    private fun formatCurrency(price:Int):String{
-        return String.format("%,d원", price)
-    }
-    private fun printStartMsg(){
+    private fun formatCurrency(price: Int) = String.format("%,d원", price)
+
+    private fun printStartMsg() {
         println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.")
     }
-    fun printEventMsg(date:Int){
+
+    fun printEventMsg(date: Int) {
         println("12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!".format(date))
     }
-    fun printMenu(menuList:List<Map<String,Int>>){
+
+    fun printMenu(menuList: List<Map<String, Int>>) {
         println("\n<주문 메뉴>")
-        menuList.flatMap { it.entries }.forEach { (key,value)-> println("$key ${value}개") }
+        menuList.flatMap { it.entries }.forEach { (key, value) -> println("$key ${value}개") }
     }
-    fun printTotalPrice(totalPrice:Int){
+
+    fun printTotalPrice(totalPrice: Int) {
         println("\n<할인 전 총주문 금액>")
         println(formatCurrency(totalPrice))
     }
-    fun printFreeGift(applicableEvents:List<String>){
+
+    fun printFreeGift(applicableEvents: List<String>) {
         println("\n<증정 메뉴>")
-        if(applicableEvents.contains(EventType.FREE_GIFT.name)) println("샴페인 1개") else println("없음")
+        if (applicableEvents.contains(EventType.FREE_GIFT.name)) println("샴페인 1개") else println("없음")
     }
-    fun printPromotionHistory(discountDetails:List<Pair<String,Int>>){
+
+    fun printPromotionHistory(discountDetails: List<Pair<String, Int>>) {
         println("\n<혜택 내역>")
-        if(discountDetails.isNotEmpty()){
+        if (discountDetails.isNotEmpty()) {
             discountDetails.forEach {
                 println("${it.first}: -${formatCurrency(it.second)}")
             }
-        }else println("없음")
+        } else println("없음")
     }
-    fun printTotalDiscount(totalDiscount:Int){
+
+    fun printTotalDiscount(totalDiscount: Int) {
         println("\n<총혜택 금액>")
         println("-${formatCurrency(totalDiscount)}")
     }
-    fun printFinalPayment(finalPayment:Int){
+
+    fun printFinalPayment(finalPayment: Int) {
         println("\n<할인 후 예상 결제 금액>")
         println(formatCurrency(finalPayment))
     }
-    fun printEventBadge(eventBadge:String){
-        var output = ""
+
+    fun printEventBadge(eventBadge: String) {
         println("\n<12월 이벤트 배지>")
-        output = if(EventType.BADGE_EVENT.promotionName.contains(eventBadge)) eventBadge else "없음"
-        println(output)
+        if(EventType.BADGE_EVENT.promotionName.contains(eventBadge)){
+            println(eventBadge)
+        }else println("없음")
     }
 }
