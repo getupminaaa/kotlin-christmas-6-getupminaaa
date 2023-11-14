f63d2679d437be799dd89bcd9e30151eae741221
getupminaaa <getupmina0405@gmail.com>
2023-11-14 13:31:57 +0900

    refactor: InputView의 getOrderMenu 메서드를 수정
    orderSheet이라는 변수를 제거하고 return에 userInput.map{}을 통해 원하는 형태의 반환을 직접 제공

--- src/main/kotlin/christmas/view/InputView.kt
+++ src/main/kotlin/christmas/view/InputView.kt
@@ -18,14 +18,12 @@ class InputView {
     fun getOrderMenu(): List<Map<String, Int>> {
         println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)")
         val userInput = Console.readLine().split(',')
-        val orderSheet = mutableListOf<Map<String,Int>>()
-        userInput.forEach {
-            isMenuMatchRegex(it)
-            val orderMenuQuantity = it.split('-')
-            orderSheet.add(mapOf(orderMenuQuantity[0] to orderMenuQuantity[1].toInt()))
-        }
+        userInput.forEach { isMenuMatchRegex(it) }
         isMenuDuplicate(userInput)
         isMenuQuantityInRange(userInput)
-        return orderSheet
+        return userInput.map {
+            val (menu, quantity) = it.split('-')
+            mapOf(menu to quantity.toInt())
+        }
     }
 }
