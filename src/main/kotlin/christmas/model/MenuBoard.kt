990872edf4c8158fcdf745c5d063b82d9a1d59bd
getupminaaa <getupmina0405@gmail.com>
2023-11-14 23:39:53 +0900

    refactor: menuBoard 클래스의 menuBoard를 enum class로 분리

--- src/main/kotlin/christmas/model/MenuBoard.kt
+++ src/main/kotlin/christmas/model/MenuBoard.kt
@@ -4,35 +4,19 @@ import christmas.model.data.MenuItem
 import christmas.util.enums.MenuCategory
 
 class MenuBoard {
-    private val menuBoard = listOf(
-        MenuItem(MenuCategory.APPETIZER.name, "양송이수프", 6000),
-        MenuItem(MenuCategory.APPETIZER.name, "타파스", 5500),
-        MenuItem(MenuCategory.APPETIZER.name, "시저샐러드", 8000),
-        MenuItem(MenuCategory.MAIN_DISH.name, "티본스테이크", 55000),
-        MenuItem(MenuCategory.MAIN_DISH.name, "바비큐립", 54000),
-        MenuItem(MenuCategory.MAIN_DISH.name, "해산물파스타", 35000),
-        MenuItem(MenuCategory.MAIN_DISH.name, "크리스마스파스타", 25000),
-        MenuItem(MenuCategory.DESSERT.name, "초코케이크", 15000),
-        MenuItem(MenuCategory.DESSERT.name, "아이스크림", 5000),
-        MenuItem(MenuCategory.BEVERAGE.name, "제로콜라", 3000),
-        MenuItem(MenuCategory.BEVERAGE.name, "레드와인", 60000),
-        MenuItem(MenuCategory.BEVERAGE.name, "샴페인", 25000)
-    )
 
-    fun isItemInMenu(itemName: String): Boolean {
-        return menuBoard.map { it.name }.contains(itemName)
-    }
+    fun isItemInMenu(itemName: String) =
+        MenuCategory.entries.flatMap { it.menuItems }.map { menuItem -> menuItem.name }.contains(itemName)
 
     fun areItemsNotOnlyBeverage(itemNames: List<String>): Boolean {
-        val beverageMenu =
-            menuBoard.filter { it.category == MenuCategory.BEVERAGE.name }.map { menuItem -> menuItem.name }
+        val beverageMenu = MenuCategory.BEVERAGE.menuItems.map { menuItem -> menuItem.name }
         return itemNames.subtract(beverageMenu.toSet()).isNotEmpty()
     }
 
     fun getMenuItems(validOrder: List<Map<String, Int>>): List<MenuItem> {
         val menuItem = mutableListOf<MenuItem>()
         validOrder.flatMap { it.keys }.forEach { name ->
-            menuItem.add(menuBoard.first { it.name == name })
+            menuItem.add(MenuCategory.entries.flatMap { it.menuItems }.first { it.name == name })
         }
         return menuItem
     }
--- src/main/kotlin/christmas/util/enums/MenuCategory.kt
+++ src/main/kotlin/christmas/util/enums/MenuCategory.kt
@@ -1,8 +1,32 @@
 package christmas.util.enums
 
-enum class MenuCategory {
-    APPETIZER,
-    MAIN_DISH,
-    DESSERT,
-    BEVERAGE
+import christmas.model.data.MenuItem
+
+enum class MenuCategory(val menuItems: List<MenuItem>) {
+    APPETIZER(
+        listOf(
+            MenuItem("APPETIZER", "양송이수프", 6000),
+            MenuItem("APPETIZER", "타파스", 5500),
+            MenuItem("APPETIZER", "시저샐러드", 8000)
+        )
+    ),
+    MAIN_DISH(
+        listOf(
+            MenuItem("MAIN_DISH", "티본스테이크", 55000),
+            MenuItem("MAIN_DISH", "바비큐립", 54000),
+            MenuItem("MAIN_DISH", "해산물파스타", 35000),
+            MenuItem("MAIN_DISH", "크리스마스파스타", 25000)
+        )
+    ),
+    DESSERT(
+        listOf(
+            MenuItem("DESSERT", "초코케이크", 15000),
+            MenuItem("DESSERT", "아이스크림", 5000),
+        )
+    ),
+    BEVERAGE(
+        listOf(
+            MenuItem("BEVERAGE", "제로콜라", 3000), MenuItem("BEVERAGE", "레드와인", 60000), MenuItem("BEVERAGE", "샴페인", 25000)
+        )
+    )
 }
