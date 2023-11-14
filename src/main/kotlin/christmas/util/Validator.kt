5682ea42d79d6f002154ac917504fd6c5ac795b5
getupminaaa <getupmina0405@gmail.com>
2023-11-14 17:07:39 +0900

    refactor: ErrorMessage를 enum class로 분리

--- src/main/kotlin/christmas/util/Validator.kt
+++ src/main/kotlin/christmas/util/Validator.kt
@@ -3,16 +3,11 @@ package christmas.util
 object Validator {
     private val dateRegex = "^([1-9]|[12]\\d|3[01])\$".toRegex()
     private val menuRegex = "^([가-힣]+)-([1-9]\\d*)$".toRegex()
-
     private const val MAX_QUANTITY = 20
 
-    fun isDateInRange(input: String) {
-        require(dateRegex.matches(input)) { "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요." }
-    }
+    fun isDateInRange(input: String) = require(dateRegex.matches(input)) { ErrorMessage.NOT_VALID_DATE.msg }
 
-    fun isMenuMatchRegex(input: String) {
-        require(menuRegex.matches(input)) { "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요." }
-    }
+    fun isMenuMatchRegex(input: String) = require(menuRegex.matches(input)) { ErrorMessage.NOT_VALID_ORDER.msg }
 
     fun isMenuQuantityInRange(inputs: List<String>) {
         var quantity = 0
@@ -20,23 +15,20 @@ object Validator {
             val matchResult = menuRegex.find(input)
             if (matchResult != null) quantity += matchResult.groupValues[2].toInt()
         }
-        require(quantity <= MAX_QUANTITY) { "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요." }
+        require(quantity <= MAX_QUANTITY) { ErrorMessage.NOT_VALID_ORDER.msg }
     }
 
     fun isMenuDuplicate(inputs: List<String>) {
         val menuNames = mutableListOf<String>()
-        for(input in inputs){
+        for (input in inputs) {
             val matchResult = menuRegex.find(input)
-            if(matchResult!=null) menuNames.add(matchResult.groupValues[1])
+            if (matchResult != null) menuNames.add(matchResult.groupValues[1])
         }
-        require(menuNames.size == menuNames.toSet().size) { "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요." }
+        require(menuNames.size == menuNames.toSet().size) { ErrorMessage.NOT_VALID_ORDER.msg }
     }
 
-    fun isOrderInMenu(isInMenu:Boolean){
-        require(isInMenu){"[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요."}
-    }
+    fun isOrderInMenu(isInMenu: Boolean) = require(isInMenu) { ErrorMessage.NOT_VALID_ORDER.msg }
 
-    fun isOrderContainsFood(notOnlyBeverage:Boolean){
-        require(notOnlyBeverage){"[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요."}
-    }
+    fun isOrderContainsFood(notOnlyBeverage: Boolean) = require(notOnlyBeverage) { ErrorMessage.NOT_VALID_ORDER.msg }
+
 }
