175a8ce3d9436f9c496b28f8398dbaeb5abd5b6b
getupminaaa <getupmina0405@gmail.com>
2023-11-14 15:40:20 +0900

    test: 금액에 따른 eventBadge값이 올바른지 확인하는 테스트 작성

--- src/test/kotlin/christmas/model/EventTest.kt
+++ src/test/kotlin/christmas/model/EventTest.kt
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test
 class EventTest {
     private val totalPrice = 142000
     private val validDate = 3
-    val event = Event(validDate, totalPrice)
+    private val event = Event(validDate, totalPrice)
 
     @Test
     @DisplayName("applicableEvent값이 올바른지 확인")
@@ -17,4 +17,16 @@ class EventTest {
         assertThat(event.applicableEvents.subtract(expected).isEmpty()).isTrue()
     }
 
+    @Test
+    @DisplayName("금액에 따른 eventBadge값이 올바른지 확인")
+    fun getEventBadgeTest() {
+        val totalDiscount = listOf(5000,9000,10000,19000,20000,30000)
+        val expected = listOf("별","별","트리","트리","산타","산타")
+        val actualValues = mutableListOf<String>()
+        totalDiscount.forEach {
+            event.getEventBadgeType(it)
+            actualValues.add(event.eventBadge)
+        }
+        assertEquals(expected,actualValues)
+    }
 }
