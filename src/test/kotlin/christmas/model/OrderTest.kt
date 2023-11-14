027a096b376965bf05b23689695d4ead6ca6ea42
getupminaaa <getupmina0405@gmail.com>
2023-11-14 14:52:01 +0900

    test: orderForms와 비교를 통해 정상적으로 값이 들어가는지 확인하는 테스트 작성

--- src/test/kotlin/christmas/model/OrderTest.kt
+++ src/test/kotlin/christmas/model/OrderTest.kt
@@ -1,19 +1,21 @@
 package christmas.model
 
 import christmas.model.data.MenuItem
+import christmas.model.data.OrderForm
 import christmas.util.MenuCategory
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
 class OrderTest {
-
-    private val menuItems = listOf(MenuItem(MenuCategory.APPETIZER.name, "타파스", 5500),
+    private val menuItems = listOf(
+        MenuItem(MenuCategory.APPETIZER.name, "타파스", 5500),
         MenuItem(MenuCategory.DESSERT.name, "초코케이크", 15000),
         MenuItem(MenuCategory.BEVERAGE.name, "레드와인", 60000),
         MenuItem(MenuCategory.MAIN_DISH.name, "해산물파스타", 35000),
         MenuItem(MenuCategory.MAIN_DISH.name, "바비큐립", 54000),
-        MenuItem(MenuCategory.APPETIZER.name, "시저샐러드", 8000))
+        MenuItem(MenuCategory.APPETIZER.name, "시저샐러드", 8000)
+    )
     private val validOrder = listOf(
         mapOf("타파스" to 1),
         mapOf("초코케이크" to 2),
@@ -22,14 +24,26 @@ class OrderTest {
         mapOf("바비큐립" to 3),
         mapOf("시저샐러드" to 1)
     )
-
     private val order = Order(menuItems, validOrder)
 
     @Test
-    @DisplayName("order의 totalPrice와 값을 비교해 계산이 정상적인지 확인")
-    fun compareTotalPrice(){
+    @DisplayName("order의 totalPrice와 비교를 통해 계산이 정상적인지 확인")
+    fun compareTotalPrice() {
         val expected = 300500
-        assertEquals(expected,order.totalPrice)
+        assertEquals(expected, order.totalPrice)
     }
 
+    @Test
+    @DisplayName("orderForms와 비교를 통해 정상적으로 값이 들어가는지 확인 ")
+    fun compareOrderForms() {
+        val expected = listOf(
+            OrderForm(MenuItem(MenuCategory.APPETIZER.name, "타파스", 5500), 1),
+            OrderForm(MenuItem(MenuCategory.DESSERT.name, "초코케이크", 15000), 2),
+            OrderForm(MenuItem(MenuCategory.BEVERAGE.name, "레드와인", 60000), 1),
+            OrderForm(MenuItem(MenuCategory.MAIN_DISH.name, "해산물파스타", 35000), 1),
+            OrderForm(MenuItem(MenuCategory.MAIN_DISH.name, "바비큐립", 54000), 3),
+            OrderForm(MenuItem(MenuCategory.APPETIZER.name, "시저샐러드", 8000), 1)
+        )
+        assertEquals(expected,order.orderForms)
+    }
 }
