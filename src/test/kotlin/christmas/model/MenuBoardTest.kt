f9300e014bfc090a3b15f9e97e265cebc825befb
getupminaaa <getupmina0405@gmail.com>
2023-11-14 14:14:19 +0900

    test: 파라미터 변경에 따른 테스트코드 변수수정

--- src/test/kotlin/christmas/model/MenuBoardTest.kt
+++ src/test/kotlin/christmas/model/MenuBoardTest.kt
@@ -1,5 +1,6 @@
 package christmas.model
 
+import christmas.model.data.MenuItem
 import christmas.util.MenuCategory
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.DisplayName
@@ -20,27 +21,48 @@ class MenuBoardTest {
     @Test
     @DisplayName("음료로만 구성되어있지 않을 경우 true, 음료로만 구성되어있을 경우 false를 반한하는지")
     fun areItemsNotOnlyBeverageTest() {
-        assertFalse { menuBoard.areItemsNotOnlyBeverage(listOf("제로콜라", "레드와인", "샴페인")) }
+        assertFalse { menuBoard.areItemsNotOnlyBeverage(listOf("제로콜라","레드와인","샴페인")) }
         assertTrue { menuBoard.areItemsNotOnlyBeverage(listOf("제로콜라", "타파스", "초코케이크")) }
         assertTrue { menuBoard.areItemsNotOnlyBeverage(listOf("티본스테이크", "타파스", "초코케이크")) }
     }
 
     @Test
     @DisplayName("입력된 메뉴들에 따른 가격을 잘 반환하는지")
-    fun getMenuItemPriceTest(){
-        val items = listOf("타파스","초코케이크","레드와인","해산물파스타","바비큐립","시저샐러드")
-        val expected = listOf(5500,15000,60000,35000,54000,8000)
-        val actualValue  = menuBoard.getMenuItemPrice(items)
-        assertEquals(expected,actualValue)
+    fun getMenuItemPriceTest() {
+        val validOrder = listOf(
+            mapOf("타파스" to 1),
+            mapOf("초코케이크" to 1),
+            mapOf("레드와인" to 1),
+            mapOf("해산물파스타" to 1),
+            mapOf("바비큐립" to 1),
+            mapOf("시저샐러드" to 1)
+        )
+        val expected = listOf(5500, 15000, 60000, 35000, 54000, 8000)
+        val actualValue = menuBoard.getMenuItemPrices(validOrder)
+        assertEquals(expected, actualValue)
     }
 
     @Test
-    @DisplayName("입력된 여러 메뉴들에 대해 메뉴 카테고리를 잘 반환하는지")
-    fun getTypesOfMenuItemsTest(){
-        val items = listOf("타파스","초코케이크","레드와인","해산물파스타","바비큐립","시저샐러드")
-        val expected = listOf(MenuCategory.APPETIZER.name,MenuCategory.DESSERT.name,MenuCategory.BEVERAGE.name,MenuCategory.MAIN_DISH.name,MenuCategory.MAIN_DISH.name,MenuCategory.APPETIZER.name)
-        val actualValue = menuBoard.getTypesOfMenuItems(items)
+    @DisplayName("입력된 여러 메뉴들에 대해 메뉴아이템을 잘 반환하는지")
+    fun getTypesOfMenuItemsTest() {
+        val validOrder = listOf(
+            mapOf("타파스" to 1),
+            mapOf("초코케이크" to 1),
+            mapOf("레드와인" to 1),
+            mapOf("해산물파스타" to 1),
+            mapOf("바비큐립" to 1),
+            mapOf("시저샐러드" to 1)
+        )
+        val expected = listOf(
+            MenuItem(MenuCategory.APPETIZER.name, "타파스", 5500),
+            MenuItem(MenuCategory.DESSERT.name, "초코케이크", 15000),
+            MenuItem(MenuCategory.BEVERAGE.name, "레드와인", 60000),
+            MenuItem(MenuCategory.MAIN_DISH.name, "해산물파스타", 35000),
+            MenuItem(MenuCategory.MAIN_DISH.name, "바비큐립", 54000),
+            MenuItem(MenuCategory.APPETIZER.name, "시저샐러드", 8000)
+        )
+        val actualValue = menuBoard.getMenuItems(validOrder)
 
-        assertEquals(expected,actualValue)
+        assertEquals(expected, actualValue)
     }
 }
